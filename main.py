23b0cbe148196bccd8e9a51a71b031f8e52e4955
Sawadogo Marcus <marcusawen@gmail.com>
2023-03-25 20:56:15 +0100

    all

---
+++ main.py
@@ -0,0 +1,23 @@
+"""To open in my_file.txt"""
+
+# with open('my_file.txt') as file:
+#   contents = file.read()
+#   print(contents)
+
+
+"""To replace the text in my_file.txt with the new text"""
+
+# with open('my_file.txt', mode='w') as file:
+#   file.write("I'm 21")
+
+"""To append my new text to in my_file.txt"""
+
+# with open('my_file.txt', mode='a') as file:
+#   file.write("\nI'm 21")
+
+"""It is possible to create a new text file just by writing this lines of code"""
+
+# with open('my_note.txt', mode='w') as file:
+#   file.write("\nI'm 21")
+
+  
